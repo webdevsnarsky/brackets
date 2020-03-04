5695a976d646f8ead043cc45aa523fe726e96daf
Alexander <snarsky93@gmail.com>
2020-03-04 18:25:54 +0300

    feat: add solution of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  return  true; 
+  let stack = [];
+  let result;
+  let arr = bracketsConfig.flat();
+  
+  for (let i = 0; i < str.length; i++) {
+    let el = str[i];
+    if(el == '(' || el == '{' || el == '[' || el == '|' && stack[stack.length - 1] != '|') {
+      stack.push(el);
+    } else {
+      if (stack.length == 0) {
+        return false;
+      }
+      if (el == ')' && stack[stack.length - 1] != '(' || 
+      el == '}' && stack[stack.length - 1] != '{' || 
+      el == '|' && stack[stack.length - 1] != '|') {
+        return false; 
+      }
+      stack.pop();
+    }
+
+  }
+  return (stack.length == 0);
 }
